c7b3b9a740728ef9d0fc9bdb5474e419c91b0ad7
ppresnyakov <ppresnyakov@yandex.ru>
2021-04-02 16:00:06 +0300

    написано первое задание

--- src/test/java/TestProperties.java
+++ src/test/java/TestProperties.java
@@ -3,9 +3,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-/**
- * Created by Maria on 07.09.2017.
- */
 public class TestProperties {
     private final Properties properties = new Properties();
 
